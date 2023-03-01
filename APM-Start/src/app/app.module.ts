f618191199c65369a921db7d31f76d840d3d9537
Celine SEBE <celine.sebe@byg4lab.com>
2023-03-01 15:17:04 +0100

    star.component

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
+import { ConvertToSpacesPipe } from './products/convert-to-spaces-pipe';
+import { StarComponent } from './shared/star.component';
 
 @NgModule({
   declarations: [
-    AppComponent, ProductListComponent
+    AppComponent, ProductListComponent, ConvertToSpacesPipe, StarComponent
   ],
   imports: [
     BrowserModule,
